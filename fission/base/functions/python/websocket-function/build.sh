22b0c5aa9b83a29b93b3b07a67ab15012d32b244
DaveW-STFC <david.white@stfc.ac.uk>
2026-03-10 10:47:21 +0000

    Updated build.sh to include extracted root folder
    Updated build.sh to cd into the extracted root folder before installing
    requirements and copying files to the deploy package.
    This ensures that the correct files are included in the deployment
    package and that the build process is successful.

--- fission/base/functions/python/websocket-function/build.sh
+++ fission/base/functions/python/websocket-function/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-cd ${SRC_PKG}/fission/base/functions/python/websocket-function
+set -e
 
-pip3 install -r  ${SRC_PKG}/requirements.txt -t  ${SRC_PKG} &&  cp -r  ${SRC_PKG} ${DEPLOY_PKG}
+cd ${SRC_PKG}/fission-functions-main/fission/base/functions/python/websocket-function
+
+pip3 install -r  requirements.txt -t  ${SRC_PKG}
+
+cp -r  ${SRC_PKG} ${DEPLOY_PKG}
